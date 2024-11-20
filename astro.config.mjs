cf2c532cb9de38b114a291c6c1df5e3e62962962
Nikita <passpuffnikita@gmail.com>
2024-11-20 11:10:27 +0300

    add adapter node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import paraglide from '@inlang/paraglide-astro';
+import node from '@astrojs/node';
 
+// https://astro.build/config
 export default defineConfig({
   // Use astro's i18n routing for deciding which language to use
   i18n: {
@@ -15,4 +17,7 @@ export default defineConfig({
       outdir: './src/paraglide', //where your files should be
     }),
   ],
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
